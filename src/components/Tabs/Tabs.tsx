595be9a5f6ecc77ac0e2464a29610eedf6bbc0d7
Jason Ransom <jason.ransom@woven-planet.global>
2025-12-05 15:43:46 -0500

    added theming support for Tabs component

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,26 +1,26 @@
 import { FC } from 'react';
 import { TabBar, TabStyle } from './tabStyles';
-import { ThemeProvider } from '@emotion/react';
-import theme from 'src/styles/theme';
 import { TabDef } from 'src/types/global';
 
 export interface TabProps {
-    /**
-     * This is to set the options which has a key and a caption.
-     */
-    options: TabDef[];
-    /**
-     * This is to set the selected tab by default.
-     */
-    value: string;
-    /**
-     * This is an event which is called whenever a tab is clicked.
-     */
-    onChange(value: string): void;
-    /**
-     * This is to set a class on the tab component.
-     */
-    className?: string;
+  /**
+   * This is to set the options which has a key and a caption.
+   */
+  options: TabDef[];
+  /**
+   * This is to set the selected tab by default.
+   */
+  value: string;
+
+  /**
+   * This is an event which is called whenever a tab is clicked.
+   */
+  onChange(value: string): void;
+
+  /**
+   * This is to set a class on the tab component.
+   */
+  className?: string;
 }
 
 /**
@@ -28,28 +28,26 @@ export interface TabProps {
  * The value will set the selection of the tab by default.
  */
 const Tabs: FC<TabProps> = props => {
-    const {
-        options,
-        value,
-        onChange,
-        className
-    } = props;
+  const {
+    options,
+    value,
+    onChange,
+    className
+  } = props;
 
-    return (
-        <ThemeProvider theme={theme}>
-            <TabBar className={className}>
-                {options.map(t =>
-                    <TabStyle
-                        key={t.key}
-                        isSelected={t.key === value}
-                        onClick={() => onChange(t.key)}
-                    >
-                        {t.caption}
-                    </TabStyle>
-                )}
-            </TabBar>
-        </ThemeProvider>
-    );
+  return (
+    <TabBar className={className}>
+      {options.map(t =>
+        <TabStyle
+          key={t.key}
+          isSelected={t.key === value}
+          onClick={() => onChange(t.key)}
+        >
+          {t.caption}
+        </TabStyle>
+      )}
+    </TabBar>
+  );
 };
 
 export default Tabs;
--- src/components/Tabs/tabStyles.ts
+++ src/components/Tabs/tabStyles.ts
@@ -3,21 +3,21 @@ import styled from '@emotion/styled';
 export const TabBar = styled.div<any>(({ theme }) => ({
     display: 'flex',
     flexFlow: 'row nowrap',
-    backgroundColor: theme?.colors?.white
+    backgroundColor: theme.backgrounds.primary
 }));
 
 
 export const TabStyle = styled.span<any>(({ theme, isSelected }) => ({
-    color: theme?.colors?.pavement,
+    color: theme.foregrounds.disabled,
     padding: '0.5rem 0 0.625rem 0',
     cursor: 'pointer',
     '& + span': {
         marginLeft: '2.5rem',
     },
     ...(isSelected) && {
-        color: theme?.colors?.cinder,
+        color: theme.foregrounds.primary,
         paddingBottom: '0.5rem',
-        borderBottom: '0.25rem solid black',
+        borderBottom: `0.25rem solid ${theme.foregrounds.primary}`,
         fontWeight: 500
     }
 }));
