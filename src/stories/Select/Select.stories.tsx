30ae49b05b8d94487c1992a26371f661d3562577
Jeff Morris <morrisjeffjr1993@gmail.com>
2026-06-10 15:36:41 -0400

    Created new disabled select story

--- src/stories/Select/Select.stories.tsx
+++ src/stories/Select/Select.stories.tsx
@@ -70,9 +70,15 @@ Select.args = {
     options: [{ label: 'Km', value: 'metric' }, { label: 'Mi', value: 'imperial' }],
     value: 'imperial',
     isMulti: false,
-  disabled: true
 };
 
+export const DisabledSelect = Template.bind({});
+DisabledSelect.args = {
+  options: [{ label: 'Km', value: 'metric' }, { label: 'Mi', value: 'imperial' }],
+  value: 'imperial',
+  isMulti: false,
+  disabled: true
+};
 export const MultiSelect = Template.bind({});
 MultiSelect.args = {
     options: [{ label: 'Km', value: 'metric' }, { label: 'Mi', value: 'imperial' }, {label: 'Made up system', value: 'made up'}],
